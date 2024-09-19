d4c7f2db147c3cb48c607dd85ee792d43fd3e205
Jose Navarro <jfnr398@gmail.co>
2024-09-19 18:28:10 -0500

    UPDATE 1

---
+++ src/main/java/Main.java
@@ -0,0 +1,337 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner lector = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        //Instancias de prueba
+        User admin = new User("José", "jfnr398", "1234");
+        User user = new User("Fernando" ,"fercho398", "4321");
+        Transaction transaction = new Ingreso(1, 6500, "pagoNomina", "Salario");
+        Transaction transaction1 = new Gasto(1, 1400, "pagoArriendo", "Arriendo");
+        menu();
+    }
+
+    public static void menu() {
+        int option;
+        do {
+            System.out.println("======MONEY MANAGER======");
+            System.out.println("1. Iniciar sesión");
+            System.out.println("2. Registrate");
+            System.out.println("3. Salir");
+            option = lector.nextInt();
+
+            switch (option) {
+                case 1:
+                    menuSesion();
+                    break;
+
+                case 2:
+                    menuRegistro();
+                    break;
+
+                case 3:
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Digite una opcion correcta");
+                    break;
+            }
+        } while (option > 3);
+    }
+
+    public static void menuSesion(){
+        System.out.println("======MONEY MANAGER======");
+        System.out.println("======INICIAR SESION=====");
+        System.out.print("Digite su correo: ");
+        String email = lector.next();
+        System.out.print("Digite su contraseña: ");
+        String password = lector.next();
+
+        Boolean status = User.validateUser(email, password);
+
+        if(status == false){
+            System.out.println("Correo o contraseña incorrectos");
+            menu();
+        }
+    }
+
+    public static void menuRegistro(){
+        System.out.println("======MONEY MANAGER======");
+        System.out.println("=========REGISTRO========");
+        System.out.print("Digite su nombre: ");
+        String name = lector.next();
+        System.out.print("Digite su correo: ");
+        String email = lector.next();
+        System.out.print("Digite su contraseña: ");
+        String password = lector.next();
+
+        User user = new User(name, email, password);
+        System.out.println("Usuario creado");
+        menu();
+    }
+
+    public static void dashboard(User user) {
+        int option;
+        do{
+            System.out.println("======MONEY MANAGER======");
+            System.out.println("========DASHBOARD========");
+            System.out.println("=========BALANCE=========");
+            System.out.println("TOTAL: " + user.sumaTotal());
+            System.out.println("En ingresos: " + user.sumaIngresos());
+            System.out.println("En gastos: " + user.sumaGastos());
+            System.out.println("======TRANSACCIONES======");
+            System.out.println("De ingresos: ");
+            user.printIngreso();
+            System.out.println("De gastos: ");
+            user.printGasto();
+            System.out.println("========OPCIONES=========");
+            System.out.println("1. Registrar ingreso");
+            System.out.println("2. Registrar gasto");
+            System.out.println("3. Modificar transacción");
+            System.out.println("4. Eliminar transaccion");
+            System.out.println("5. Consultar cuenta");
+            System.out.println("=========================");
+            option = lector.nextInt();
+
+            if(option == 1 || option == 2){
+                menuTransaccion(option, user);
+            } else if(option == 3){
+                updateTransaction(user);
+            } else if(option == 4){
+                user.getTransactionOnList();
+                deleteTransaction(user);
+            } else if(option == 5){
+                selectUser(user);
+            }
+        }while(option > 3);
+    }
+
+    private static void menuTransaccion(int type, User user){
+        int option = 0;
+        int register = 0;
+        System.out.println("======MONEY MANAGER======");
+        System.out.println("=====REG TRANSACCION=====");
+
+        if(type == 3){
+            System.out.println(user.selectTransaction);
+            System.out.println("Digite el tipo de transacción: ");
+            System.out.println("1. Ingreso");
+            System.out.println("2. Gasto");
+            register = lector.nextInt();
+        }
+        System.out.print("Digite un valor: ");
+        int value = lector.nextInt();
+        System.out.print("Digite una descripción: ");
+        String description = lector.next();
+        lector.nextLine();
+
+        String category = "";
+
+        do{
+            System.out.println("Escoja una categoria: ");
+            System.out.println("1. Salario");
+            System.out.println("2. Arriendo");
+            System.out.println("3. Comisiones");
+            System.out.println("4. Servicios");
+            option = lector.nextInt();
+
+            switch (option){
+                case 1:
+                    category = "Salario";
+                    break;
+
+                case 2:
+                    category = "Arriendo";
+                    break;
+
+                case 3:
+                    category = "Comisiones";
+                    break;
+
+                case 4:
+                    category = "Servicios";
+                    break;
+
+                default:
+                    System.out.println("Digite una opcion correcta");
+                    break;
+            }
+        }while(option > 4);
+
+        switch (type){
+            case 1:
+                user.createTransaction("Ingreso", value, description, category);
+                break;
+
+            case 2:
+                user.createTransaction("Gasto", value, description, category);
+                break;
+
+            case 3: //Modificar transacción
+                if(register == 1){
+                    user.updateTransaction("Ingreso", value, description, category);
+                } else if (register == 2){
+                    user.updateTransaction("Gasto", value, description, category);
+                }
+                break;
+        }
+
+        dashboard(user);
+    }
+
+    public static void updateTransaction(User user){
+        int option;
+        do {
+            System.out.println("======MONEY MANAGER======");
+            System.out.println("========MODIFICAR========");
+            user.printTransactionOnList();
+            System.out.print("Digite el número de la transacción a modificar: ");
+            option = lector.nextInt();
+
+            if(option > user.getTransactionOnList().size()){
+                System.out.println("Digite un número valido");
+            }
+        }while (option > user.getTransactionOnList().size());
+
+        user.takeTransaction(option);
+        menuTransaccion(3, user);
+    }
+
+    public static void deleteTransaction(User user){
+        int option;
+        do{
+            System.out.println("======MONEY MANAGER======");
+            System.out.println("========ELIMINAR========");
+            user.printTransactionOnList();
+            System.out.print("Digite el número de la transacción a eliminar: ");
+            option = lector.nextInt();
+
+            if(option > user.getTransactionOnList().size()){
+                System.out.println("Digite un número valido");
+            }
+        }while(option > user.getTransactionOnList().size());
+
+        user.takeTransaction(option);
+        int confirm;
+
+        do {
+            System.out.println("Estas seguro de eliminar esta transacción");
+            System.out.println(user.selectTransaction);
+            System.out.println("1. Si");
+            System.out.println("2. No");
+            confirm = lector.nextInt();
+
+            switch (confirm) {
+                case 1:
+                    System.out.println("Transacción eliminada");
+                    user.deleteTransaction();
+                    dashboard(user);
+                    break;
+
+                case 2:
+                    dashboard(user);
+                    break;
+
+                default:
+                    System.out.println("Opción no valida");
+                    break;
+            }
+        }while (confirm > 2);
+    }
+
+    public static void selectUser(User user){
+        int option;
+        do{
+            System.out.println("======MONEY MANAGER======");
+            System.out.println("======CUENTA USUARIO=====");
+            System.out.println("Nombre: " + user.getName());
+            System.out.println("Email: " + user.getEmail());
+            System.out.println("Ingresos registrados: " + user.getTotalIngreso());
+            System.out.println("Gastos registrados: " + user.getTotalGasto());
+            System.out.println("1. Dashboard");
+            System.out.println("2. Modificar datos");
+            System.out.println("3. Cerrar sesión");
+            System.out.println("=========================");
+            option = lector.nextInt();
+
+            switch (option){
+                case 1:
+                    dashboard(user);
+                    break;
+
+                case 2:
+                    updateUser(user);
+                    break;
+
+                case 3:
+                    user.logout();
+
+                default:
+                    System.out.println("Digite una opcion correcta");
+                    break;
+            }
+        }while(option > 3);
+    }
+
+    public static void updateUser(User user){
+        int option;
+        do {
+            System.out.println("======MONEY MANAGER======");
+            System.out.println("====MODIFICAR USUARIO====");
+            System.out.println("1. Cambiar nombre: " + user.getName());
+            System.out.println("2. Cambiar email: " + user.getEmail());
+            System.out.println("3. Cambiar contraseña: " + user.getPassword());
+            System.out.println("4. Consultar cuenta");
+            option = lector.nextInt();
+
+            switch (option){
+                case 1:
+                    System.out.print("Digite un nuevo nombre: ");
+                    String name = lector.next();
+
+                    System.out.println("Su nombre ha sido cambiado");
+                    user.setName(name);
+                    break;
+
+                case 2:
+                    System.out.print("Digite un nuevo email: ");
+                    String email = lector.next();
+
+                    System.out.println("Su correo ha sido cambiado");
+                    user.setEmail(email);
+                    break;
+
+                case 3:
+                    String password;
+                    String passwordConfirm;
+                    do{
+                        System.out.print("Digite una nueva contraseña: ");
+                        password = lector.next();
+                        System.out.print("Confirme su nueva contraseña: ");
+                        passwordConfirm = lector.next();
+
+                        if(password.equals(passwordConfirm)){
+                            System.out.println("Su contraseña ha sido cambiada");
+                            user.setPassword(password);
+                            dashboard(user);
+                        }else {
+                            System.out.println("Las contraseñas no coinciden");
+                        }
+                    }while (!password.equals(passwordConfirm));
+                    break;
+
+                case 4:
+                    selectUser(user);
+                    break;
+
+                default:
+                    System.out.println("Opción no valida");
+                    break;
+            }
+        }while (option > 4);
+    }
+}
---
+++ src/main/java/User.java
@@ -0,0 +1,232 @@
+import java.util.ArrayList;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String type;
+    private String bank;
+    private double balance;
+
+    private static int userCounter;
+    public static ArrayList<User> datos = new ArrayList<>();
+    public Transaction selectTransaction;
+    //Atributos static: Se comparten entre todas las instancias. Solo existe una copia de la variable para toda la clase, por lo que las listas es ideal que solo exista una copia ya que aquí se guardarán a todos los usuarios
+    //Métodos static: Pueden ser llamados sin necesidad de crear una instancia de la clase.
+
+    public ArrayList<Transaction> ingresoUser = new ArrayList<Transaction>();
+    public ArrayList<Transaction> gastoUser = new ArrayList<Transaction>();
+
+    public User(String name, String email, String password) {//CreateUser - Constructor
+        this.id = ++userCounter;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        datos.add(this);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public int getTotalIngreso(){
+        return ingresoUser.size();
+    }
+
+    public int getTotalGasto(){
+        return gastoUser.size();
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public static Boolean validateUser(String email, String password){
+        for (int i = 0; i < datos.size(); i++) {
+            if(email.equals(datos.get(i).email) && password.equals(datos.get(i).password)){
+                System.out.println("Acceso autorizado.");
+                System.out.println("Bienvenido " + datos.get(i).name);
+                Main.dashboard(datos.get(i));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public double sumaIngresos(){
+        loadIngreso();
+
+        double total = 0;
+        for (int i = 0; i < ingresoUser.size(); i++) {
+            total += ingresoUser.get(i).getValue();
+        }
+        return total;
+    }
+
+    public double sumaGastos(){
+        loadGasto();
+
+        double total = 0;
+        for (int i = 0; i < gastoUser.size(); i++) {
+            total += gastoUser.get(i).getValue();
+        }
+        return total;
+    }
+
+    public double sumaTotal() {
+        double ingreso = sumaIngresos();
+        double gasto = sumaGastos();
+        double total = ingreso - gasto;
+        return total;
+    }
+
+    public void loadIngreso(){
+        Transaction.userIngreso(this.id, this.ingresoUser);
+    }
+
+    public void loadGasto(){
+        Transaction.userGasto(this.id, this.gastoUser);
+    }
+
+    public void printIngreso(){
+        this.ingresoUser.forEach(System.out::println);
+    }
+
+    public void printGasto(){
+        this.gastoUser.forEach(System.out::println);
+    }
+
+    public void updateUser(){
+
+    };
+
+    public void createTransaction(String type, double value, String description, String category){
+        if(type.equals("Ingreso")){
+            Ingreso transaction = new Ingreso(this.id, value, description, category);
+        } else if(type.equals("Gasto")){
+            Gasto transaction = new Gasto(this.id, value, description, category);
+        }
+
+        Transaction.printData();
+    }
+
+    public ArrayList<Transaction> getTransactionOnList(){ //Permite tener todas las transacciones almacenadas de ingresoUser y gastoUser
+        ArrayList<Transaction> transaction = new ArrayList<Transaction>();
+
+        for (int i = 0; i < ingresoUser.size(); i++) {
+            transaction.add(ingresoUser.get(i));
+        }
+
+        for (int i = 0; i < gastoUser.size(); i++) {
+            transaction.add(gastoUser.get(i));
+        }
+
+        return transaction;
+    }
+
+    public void printTransactionOnList(){
+        ArrayList<Transaction> transactionList = getTransactionOnList();
+        for (int i = 0; i < transactionList.size(); i++) {
+            System.out.println(i+1 + ". " + transactionList.get(i));
+        }
+    }
+
+    public void takeTransaction(int option){ //Obtiene la transacción especifica de la lista en donde se guardarán todas las transacciones del usuario para seguidamente buscar esta transacción seleccionada desde la base de datos de la transacciones
+        ArrayList<Transaction> transactionList = getTransactionOnList();
+
+        Transaction transaction = transactionList.get(option - 1);
+
+        for (int i = 0; i < Transaction.ingresos.size(); i++) {
+            if(transaction == Transaction.ingresos.get(i)){
+                selectTransaction = Transaction.ingresos.get(i);
+            }
+        }
+
+        for (int i = 0; i < Transaction.gastos.size(); i++) {
+            if(transaction == Transaction.gastos.get(i)){
+                selectTransaction = Transaction.gastos.get(i);
+            }
+        }
+
+    }
+
+    public void updateTransaction(String type, int value, String description, String category){
+        if(selectTransaction.getType() != type){
+            if(type == "Ingreso"){
+                Transaction.ingresos.add(selectTransaction);
+
+                for (int i = 0; i < Transaction.gastos.size(); i++) {
+                    if(selectTransaction == Transaction.gastos.get(i)){
+                        Transaction.gastos.remove(i);
+                    }
+                }
+            }else if(type == "Gasto"){
+                Transaction.gastos.add(selectTransaction);
+
+                for (int i = 0; i < Transaction.ingresos.size(); i++) {
+                    if(selectTransaction == Transaction.ingresos.get(i)){
+                        Transaction.ingresos.remove(i);
+                    }
+                }
+            }
+        }
+
+        selectTransaction.setType(type);
+        selectTransaction.setValue(value);
+        selectTransaction.setDescription(description);
+        selectTransaction.setCategory(category);
+
+        Transaction.printData();
+    }
+
+    public void deleteTransaction(){
+        for (int i = 0; i < Transaction.ingresos.size(); i++) {
+            if(selectTransaction == Transaction.ingresos.get(i)){
+                Transaction.ingresos.remove(i);
+                break;
+            }
+        }
+
+        for (int i = 0; i < Transaction.gastos.size(); i++) {
+            if(selectTransaction == Transaction.gastos.get(i)){
+                Transaction.gastos.remove(i);
+                break;
+            }
+        }
+
+        Transaction.printData();
+    }
+
+    public void logout(){
+        Main.menu();
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+
+}
